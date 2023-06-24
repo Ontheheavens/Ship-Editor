f659dc904ed7c699be31089d338c82159b376e0b
Ontheheavens <l2van876b@gmail.com>
2023-06-24 12:03:38 +0400

    Behaviour fixes for tabbed split-pane minimizer and divider.

---
+++ src/main/java/oth/shipeditor/components/LeftsideTabType.java
@@ -0,0 +1,9 @@
+package oth.shipeditor.components;
+
+/**
+ * @author Ontheheavens
+ * @since 24.06.2023
+ */
+enum LeftsideTabType {
+    DEFAULT, GAME_DATA
+}
--- src/main/java/oth/shipeditor/components/TripleSplitContainer.java
+++ src/main/java/oth/shipeditor/components/TripleSplitContainer.java
@@ -16,6 +16,9 @@ import javax.swing.plaf.basic.BasicSplitPaneDivider;
 import java.awt.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Ontheheavens
@@ -40,45 +43,61 @@ final class TripleSplitContainer extends JSplitPane {
      */
     private InstrumentTabsPane instrumentPane;
 
-    private final JPanel defaultLeftsidePanel;
-
-    private GameDataPanel gameDataPanel;
-
     private FlatArrowButton dividerLeftButton;
 
     private FlatArrowButton dividerRightButton;
 
     private final MinimizerWidget minimizer;
 
+    private final Map<LeftsideTabType, JPanel> leftsidePanels;
+
+    private final Map<Component, Integer> dividerLocations;
+
     TripleSplitContainer(JTabbedPane westPane) {
         super((JSplitPane.HORIZONTAL_SPLIT));
         this.setOneTouchExpandable(true);
         this.putClientProperty("JSplitPane.expandableSide", "right");
-        this.initListeners();
+        this.dividerLocations = new HashMap<>();
+        this.initEventListeners();
         this.minimizer = new MinimizerWidget(getMinimizeAction(), getMaximizeAction());
-        defaultLeftsidePanel = new JPanel();
-        defaultLeftsidePanel.add(new JLabel("Default"));
+        leftsidePanels = new EnumMap<>(LeftsideTabType.class);
+        leftsidePanels.put(LeftsideTabType.DEFAULT, new JPanel());
+        JPanel panel = leftsidePanels.get(LeftsideTabType.DEFAULT);
+        panel.add(new JLabel(WindowContentPanes.DEFAULT_LEFTSIDE_PANE));
+        this.initDividerListeners(westPane);
+    }
+
+    private void initDividerListeners(JTabbedPane westPane) {
         westPane.addChangeListener(e -> {
             WindowContentPanes.LeftsidePanelTab selected = (WindowContentPanes.LeftsidePanelTab) westPane.getSelectedComponent();
+            Component toSelect = null;
             switch (selected.getTabType()) {
-                case DEFAULT -> this.setLeftComponent(defaultLeftsidePanel);
-                case GAME_DATA -> this.setLeftComponent(gameDataPanel);
+                case DEFAULT -> toSelect = leftsidePanels.get(LeftsideTabType.DEFAULT);
+                case GAME_DATA -> toSelect = leftsidePanels.get(LeftsideTabType.GAME_DATA);
+            }
+            this.setLeftComponent(toSelect);
+            if (dividerLocations.get(toSelect) != null) {
+                int location = dividerLocations.get(toSelect);
+                this.setDividerLocation(Math.max(location, this.getMinimumDividerLocation()));
             }
             if (minimizer.isMinimized()) {
                 minimizer.setRestorationQueued(true);
             }
             minimizer.setPanelSwitched(true);
         });
-        // Minimizing behaviour is still subpar for these panels, but we have more pressing matter to deal with.
-        // Meticulous logging and close examination of boolean interactions will likely solve the oddities.
         westPane.addMouseListener(new MinimizeListener(westPane, minimizer));
+        this.addPropertyChangeListener(JSplitPane.DIVIDER_LOCATION_PROPERTY, evt -> {
+            int location = getDividerLocation();
+            Component selected = this.getLeftComponent();
+            dividerLocations.put(selected, location);
+        });
     }
 
     private Runnable getMinimizeAction() {
         return () -> {
             if (dividerLeftButton.isVisible()) {
-                dividerLeftButton.doClick();
                 minimizer.setMinimized(true);
+                dividerLeftButton.doClick();
             }
         };
     }
@@ -86,13 +105,16 @@ final class TripleSplitContainer extends JSplitPane {
     private Runnable getMaximizeAction() {
         return () -> {
             if (dividerRightButton.isVisible()) {
+                minimizer.setMinimized(false);
                 dividerRightButton.doClick();
+            }
+            else if (minimizer.isRestorationQueued() && minimizer.isMinimized()) {
                 minimizer.setMinimized(false);
             }
         };
     }
 
-    private void initListeners() {
+    private void initEventListeners() {
         EventBus.subscribe(event -> {
             if (event instanceof InstrumentSplitterResized checked) {
                 boolean minimize = checked.minimized();
@@ -151,9 +173,11 @@ final class TripleSplitContainer extends JSplitPane {
                 }
             }
         }
-        gameDataPanel = new GameDataPanel();
+        dividerLeftButton.addActionListener(e -> minimizer.setMinimized(true));
+        dividerRightButton.addActionListener(e -> minimizer.setMinimized(false));
+        leftsidePanels.put(LeftsideTabType.GAME_DATA, new GameDataPanel());
         secondaryLevel.setMinimumSize(new Dimension(480, this.getHeight()));
-        this.setLeftComponent(gameDataPanel);
+        this.setLeftComponent(leftsidePanels.get(LeftsideTabType.GAME_DATA));
         this.setRightComponent(secondaryLevel);
         this.setResizeWeight(0.1f);
     }
